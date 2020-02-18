12f6ad48282e8a07256b44ff6e735cf581b38675
jony <jonytrees@gmail.com>
2020-02-18 20:42:38 +0200

    update form code

--- src/components/form.js
+++ src/components/form.js
@@ -1,25 +1,88 @@
-import React from "react"
+import React, { Component } from 'react'
 import $ from 'jquery'
-import { withPrefix } from 'gatsby'
 
 
 class Form extends React.Component {
+  state = {
+    inputEmail: '',
+    inputCheckBoth: false,
+    inputCheckDesign: false,
+    inputCheckDev: false,
+    inputMessage: '',
+    inputName: '',
+    isCaptchaValid: false,
+    isErrorShown: false,
+    isFormValid: false
+  }
+
+  handleInput = event => {
+    // Test for input and length of the value
+    if (event.target.value.length > 0 && event.target.name !== 'inputEmail') {
+      this.setState({
+        [event.target.name]: event.target.value
+      })
+    }
+
+    // If input is for email address validate it with regexp
+    if (event.target.name === 'inputEmail') {
+      // eslint-disable-next-line
+      const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+      if (reg.test(String(event.target.value).toLowerCase())) {
+        this.setState({
+          [event.target.name]: event.target.value
+        })
+      }
+    }
+  }
+
+
+  // Show message in console when reCaptcha plugin is loaded
+  onCaptchaLoad = () => {
+    console.log('Captcha loaded')
+  }
+
+
+
   handleFormSubmit = event => {
     event.preventDefault()
 
-    // Test required fields - email and name
-    if (this.state.email.length > 0 && this.state.name.length > 0) {
-      // Send the data with Ajax and jQuery
+    // Test
+    if (this.state.inputEmail.length > 0 && this.state.inputName.length > 0 && this.state.inputMessage.length > 0 && this.state.isCaptchaValid) {
+      this.setState({
+        isErrorShown: false,
+        isFormValid: true
+      })
+
+      // Send the form with AJAX
       $.ajax({
         data: this.state,
         type: 'POST',
-        url: withPrefix('/sendEmail.php'), // use 'withPrefix' module from 'gatsby' to reference 'sendEmail.php' in 'static' folder.
+        url: '../../sendEmail.php',
         success: function(data) {
           console.info(data)
         },
         error: function(xhr, status, err) {
           console.error(status, err.toString())
         }
+      })
+
+      // Reset state after sending the form
+      this.setState({
+        inputEmail: '',
+        inputCheckBoth: false,
+        inputCheckDesign: false,
+        inputCheckDev: false,
+        inputMessage: '',
+        inputName: '',
+        isCaptchaValid: false,
+        isErrorShown: false,
+        isFormValid: false
+      })
+    } else {
+      // Show error message
+      this.setState({
+        isErrorShown: true
       })
     }
   }
@@ -34,26 +97,26 @@ class Form extends React.Component {
             <div className="form-blocks">
               <p>Contact details</p>
               <div className="form-div">
-                <input type="text" name="fio" id="fio" className="required" />
+                <input type="text" name="fio" onChange={this.handleInput} required={true} id="fio" className="required" />
                   <label htmlFor="fio">Full Name*</label>
                 </div>
                 <div className="form-div">
-                  <input type="text" name="company" id="company" />
+                  <input type="text" name="company" onChange={this.handleInput} required={true} id="company" />
                     <label htmlFor="company">Company / Organization</label>
                   </div>
                   <div className="form-div">
-                    <input type="text" name="phone" id="phone" />
+                    <input type="text" name="phone" onChange={this.handleInput} required={true} id="phone" />
                       <label htmlFor="phone">Phone</label>
                     </div>
                     <div className="form-div">
-                      <input type="text" name="email" id="email" className="required" />
+                      <input type="text" name="email" onChange={this.handleInput} required={true} id="email" className="required" />
                         <label htmlFor="email">Email*</label>
                       </div>
                     </div>
                     <div className="form-blocks">
                       <p>Request details</p>
                       <div className="form-div">
-                        <textarea name="message" id="message" className="required"></textarea>
+                        <textarea name="message" onChange={this.handleInput} required={true} id="message" className="required"></textarea>
                         <label htmlFor="message">Please describe your requirements*</label>
                       </div>
                       <div className="form-div">
